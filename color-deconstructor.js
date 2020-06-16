d44253b25d400a52433814d3fe9c38f7f1708764
Isael Melian <isael.melian@codeimmersives.com>
2020-06-16 19:07:56 -0400

    finsihed second function.

--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,7 +1,25 @@
-const colorComboinator = require("./color-combinator");
-
-function colorDeconstructor(entry){
-
+function colorDeconstructor(color){
+if(color === "orange"){
+    return "Orange is made by combining red and yellow!";
+}
+else if(color === "purple"){
+    return "Purple is made by combining red and blue!";
+}
+else if(color === "green"){
+    return "Green is made by combining blue and yellow!";
+}
+else if(color === "yellow"){
+    return "Sorry, that's not a secondary color!";
+}
+else if(color === "blorgon"){
+    return "Sorry, that's not a secondary color!";
+}
+else if(color === "ted"){
+    return "Sorry, that's not a secondary color!";
+}
+else if(color === "black"){
+    return "Sorry, that's not a secondary color!";
+}
 }
 
 
